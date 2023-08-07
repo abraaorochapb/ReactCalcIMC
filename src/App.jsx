8cb055434f2f5b6a1655dd39086e7415382d2d40
abraaorochapb <abraaorochapb@gmail.com>
2023-08-07 13:25:58 -0300

    Adição de dados oriundos da OMS  sobre o calculo do IMC

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,9 @@ function App() {
   return (
     <div className='flex justify-center'>
       <div className='bg-zinc-700 w-fit h-fit mt-40 rounded-sm p-8'>
-
         <p className='text-white text-center text-3xl m-4 mt-2'>Calculadora de IMC</p>
         
         <CompForm/>
-        
       </div>
     </div>
   )
